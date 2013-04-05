10881366e659a18fb12d93af9795a1bb1292bbcc
Fabrizio Spada <fabriziospada@host-001.homenet.telecomitalia.it>
2013-04-05 11:45:40 +0200

    correction to xmlLoader

--- scripts/xmlLoader.py
+++ scripts/xmlLoader.py
@@ -2,15 +2,11 @@ import types
 #import gc
 
 class XmlLoader():
-	class XmlTree():
-		def __init__(self):
-			self.subtree = list()
-			self.data = None
 
 	def __init__(self,filename):
 		self.filename = filename
 		self.fd = None
-		self.rootXml = XmlLoader.XmlTree()
+		self.subclasses=list()
 		self.allClass = dict()
 		self.allClassName = list()
 		self.i=0
@@ -35,15 +31,14 @@ class XmlLoader():
 					# if not exist create class in self.allClass with type()
 					if words[0][1:] not in self.allClassName:
 						self.allClassName.append(words[0][1:])
-						self.allClass[words[0][1:]] = type(words[0][1:],(),{})						
+						self.allClass[words[0][1:]] = type(words[0][1:],(),{'subclasses':list()})					
 					#create an instance of class with attrs and methods, load in current rootXml
-					t = XmlLoader.XmlTree()
-					t.data = (self.allClass[words[0][1:]])()
+					t = (self.allClass[words[0][1:]])()
 					for w in words[1:len(words)-1]:
 						att=w.split('=')[0]
 						val=w.split('"')[1]
-						t.data.__dict__[att]=val
-					rootXml.subtree.append(t)
+						t.__dict__[att]=val
+					rootXml.subclasses.append(t)
 					if words[-1][-1] == '>' and len(words[-1])==1:
 						# continue with depth search
 						self.__analize(t)
@@ -71,7 +66,7 @@ class XmlLoader():
 			print "error: impossible to open file " + "\""+e.filename+"\""
 			return False
 		print str(self.perc)+"%"
-		self.__analize(self.rootXml)
+		self.__analize(self)
 		self.fd.close()
 		return True
 
